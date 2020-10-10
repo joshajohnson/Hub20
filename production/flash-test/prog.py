df5e106c623920218a5048c27c208dbf19874ac3
Josh Johnson <josh@joshajohnson.com>
2020-10-10 17:48:45 +1100

    update test script + enclosure files

---
+++ production/flash-test/prog.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+
+# Script to program and test Hub20
+
+import os
+import time
+import argparse
+
+def qmk():
+    # Flash QMK
+    flash = "dfu-util -d 0483:df11 -a 0 -s 0x08000000:leave -D ../../firmware/binaries/hub20_via.bin"
+    os.system(flash)
+
+def read():
+    # Read entire firmware, including VIA EEPROM emulation
+    read = "dfu-util -d 0483:df11 -a 0 -s 0x08000000:131072 -U firmware.bin"
+    os.system(read)
+
+def test():
+    # Test Device
+    chars_reqd = "abcdefghijklmnopqrstuvwxyz"
+    chars_input = input("Press all the keys!: ")
+    chars_remain = ""
+
+    while True:
+
+        for char in chars_reqd:
+            if char in chars_input:
+                chars_remain = chars_remain.replace(char, "")
+            elif char not in chars_remain:
+                chars_remain = chars_remain + char
+
+        if len(chars_remain) == 0:
+            print("ALL KEYS WORKING")
+            break
+
+        chars_reqd = chars_remain
+        chars_input = input("Need to press {}: ".format(chars_remain))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--qmk",          action='store_true')
+    parser.add_argument("-t", "--test",         action='store_true')
+    parser.add_argument("-r", "--repeat",       action='store_true')
+    args = parser.parse_args()
+
+    while True:
+
+	    if args.qmk:
+	        qmk()
+	    if args.test:
+	        test()
+
+
+	    if args.repeat == False:
+	    	break
+	    time.sleep(1)
+
+	    input("Press any key to continue.")
