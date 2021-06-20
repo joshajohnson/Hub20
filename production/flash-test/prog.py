2e41f9769b3559190b7f393e920120bb8105d0a0
Josh Johnson <josh@joshajohnson.com>
2021-06-20 20:55:11 +1000

    update test files

--- production/flash-test/prog.py
+++ production/flash-test/prog.py
@@ -5,27 +5,32 @@
 import os
 import time
 import argparse
+from colorama import Fore
 
 def qmk():
     # Flash QMK
-    flash = "dfu-util -d 0483:df11 -a 0 -s 0x08000000:leave -D ../../firmware/binaries/hub20_via.bin"
-    os.system(flash)
+    qmk = "dfu-util -d 0483:df11 -a 0 -s 0x08000000:leave -D hub20_prod.bin"
+    retval = os.system(qmk)
+
+def test_fw():
+    # Flash Test Firmware
+    test = "dfu-util -d 0483:df11 -a 0 -s 0x08000000:leave -D hub20_test.bin"
+    retval = os.system(test)
 
 def read_firmware():
     # Read entire firmware, including VIA EEPROM emulation
     read = "dfu-util -d 0483:df11 -a 0 -s 0x08000000:131072 -U firmware.bin"
-    os.system(read)
-
+    retval = os.system(read)
 
 def write_firmware():
     # Write entire firmware, including VIA EEPROM emulation
     write = "dfu-util -d 0483:df11 -a 0 -s 0x08000000:leave -D firmware.bin"
-    os.system(write)
+    retval = os.system(write)
 
 def test():
     # Test Device
     chars_reqd = "abcdefghijklmnopqrstuvwxyz"
-    chars_input = input("Press all the keys!: ")
+    chars_input = input("Check LEDs are functioning, then press all the keys!: ")
     chars_remain = ""
 
     while True:
@@ -46,17 +51,26 @@ def test():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--qmk",          action='store_true')
-    parser.add_argument("-e", "--read_eeprom",  action='store_true')
-    parser.add_argument("-w", "--write_eeprom", action='store_true')
+    parser.add_argument("-f", "--test_fw",     action='store_true')
     parser.add_argument("-t", "--test",         action='store_true')
+    parser.add_argument("-q", "--qmk",          action='store_true')
+    parser.add_argument("-r", "--read_eeprom",  action='store_true')
+    parser.add_argument("-w", "--write_eeprom", action='store_true')
     args = parser.parse_args()
 
+    if args.test_fw:
+        retval = input("Reset Hub20, then press enter to continue")
+        test_fw()
+    if args.test:
+        test()
     if args.qmk:
+        retval = input("Reset Hub20, then press enter to continue")
         qmk()
     if args.read_eeprom:
         read_firmware()
     if args.write_eeprom:
         write_firmware()
-    if args.test:
-        test()
+
+    print(f'{Fore.GREEN}###################')
+    print(f'{Fore.GREEN}ALL STEPS PASSED!!!')
+    print(f'{Fore.GREEN}###################')
